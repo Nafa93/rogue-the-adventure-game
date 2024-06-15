f4f9c8128d8c0f30155e4580af753958158c1327
Nicolás Alejandro Fernández Amorosino <nicolasfernandez93@gmail.com>
2024-06-15 01:48:53 -0300

    Added renders for room and hud

--- rogue-the-adventure-game/rogue-the-adventure-game/rogue-the-adventure-game.cpp
+++ rogue-the-adventure-game/rogue-the-adventure-game/rogue-the-adventure-game.cpp
@@ -1,11 +1,70 @@
-// rogue-the-adventure-game.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// rogue-the-adventur;e-game.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
 #include <iostream>
 
+using namespace std;
+
+class Player
+{
+public:
+    int level;
+    int hitPoints;
+    int currentHitPoints;
+    int strength;
+    int currentStrength;
+    int gold;
+    int armor;
+    int experience;
+    int currentExperience;
+
+    Player()
+    {
+        level = 1;
+        hitPoints = 100;
+        currentHitPoints = 100;
+        strength = 10;
+        currentStrength = 10;
+        gold = 0;
+        armor = 10;
+        experience = 10;
+        currentExperience = 0;
+    }
+};
+
+void render_hud(Player player)
+{
+    printf("Level: %d Hits:%d(%d) Str:%d(%d) Gold:%d Armor:%d Exp:%d/%d", player.level, player.currentHitPoints, player.hitPoints, player.currentStrength, player.strength, player.gold, player.armor, player.currentExperience, player.experience);
+}
+
+void render_canvas()
+{
+}
+
+void render_room(int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if ((i == 0) || (j == 0) || (i == height - 1) || (j == width - 1))
+            {
+                printf("#");
+            }
+            else
+            {
+                printf("-");
+            }
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Player player;
+    //render_hud(player);
+    render_room(5, 10);
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
